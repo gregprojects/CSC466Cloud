25a959da90489e6c19fbec8e5020f1dc17298c46
gregprojects <54902309+gregprojects@users.noreply.github.com>
2020-12-03 10:37:56 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -38,11 +38,7 @@ for i in range(3):
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
 
-  if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/nfs/nfs-server.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_mpi.sh"))
-  else: 
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/nfs/nfs-client.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_mpi.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
